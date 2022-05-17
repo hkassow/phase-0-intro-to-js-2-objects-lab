126d04e6a551fd80b7c56279e930323735fc8c07
Hunter Kassow <hunterkassow@protonmail.com>
2022-05-17 10:43:10 -0400

    commit tree 15e334bdf1e413a0b7129410706b2d2d81e2661a

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+let employee = {
+    name: 'Sam'
+}
+employee['street address'] = '11 Broadway';
+
+function updateEmployeeWithKeyAndValue(employee, key, object) {
+    const newEmployee = {...employee};
+    newEmployee[key] = object;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){ 
+    const newEmployee = {...employee}
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
